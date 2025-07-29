b89ab3f5db359dcc83fda90e6dd26f7ddbc78808
Ari007m <ariharasudan007@gmail.com>
2025-07-29 18:14:50 +0530

    Hangman game Started

---
+++ hangman/src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import words from './wordList.json'
+import { HangmanDrawing } from './HangmanDrawing';
+import { HangmanWord } from './HangmanWord';
+import { Keyboard } from './Keyboard';
+
+/* https://www.youtube.com/watch?v=-ONUyenGnWw
+https://github.com/WebDevSimplified/react-hangman/tree/main 
+*/
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random()*words.length)]
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  const inCorrectLetters = guessedLetters.filter(
+    letter => !wordToGuess.includes(letter)
+  )
+
+  return (
+    <>
+    <div style={{
+      maxWidth : "800px",
+      display : "flex",
+      flexDirection : "column",
+      gap : "2rem",
+      margin : "0 auto",
+      alignItems : "center"
+    }}>
+      <div style={{fontSize : "2rem", textAlign : "center"}}>Lose Win</div>
+      <HangmanDrawing numberOfGuesses = {inCorrectLetters.length}/>
+      <HangmanWord />
+      <div style={{alignSelf: "stretch"}}>
+        <Keyboard/>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
