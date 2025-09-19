fdf5eecf3ffd39382c4b6ef9e82fa965138db3f7
Ari007m <ariharasudan007@gmail.com>
2025-09-19 17:24:32 +0530

    Use Memo and Use Callback started

--- native-useMemoAnduseCallback/src/App.jsx
+++ native-useMemoAnduseCallback/src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo, useState } from 'react'
+
+function SortedNames({list}){
+  const sortedNames =useMemo(() => {
+    return [...list].sort();
+  },[list]);
+
+  return(
+    <div>
+      {sortedNames.join(", ")}
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [numbers] = useState([10,20,30]);
+  const total = useMemo(() => numbers.reduce((acc,number) => acc+number,0),[numbers]);
+
+  const [names] = useState(["Ari","john","bill"]);
+
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const countTot = useMemo(() => count1+count2 ,[count1,count2]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Total : {total}</div>
+      <div>Names : {names.join(", ")}</div>
+      <div>sortedNames : {<SortedNames list={names}/>}</div>
+      <button onClick={() => {setCount1(count1+1)}}>Count1 : {count1}</button>
+      <button onClick={() => {setCount2(count2+1)}}>Count2 : {count2}</button>
+      <div>Total Count: {countTot}</div>
     </>
   )
 }
