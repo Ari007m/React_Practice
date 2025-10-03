42fb1059c01789cef477be75a4778151180902cf
Ari007m <ariharasudan007@gmail.com>
2025-10-03 17:57:14 +0530

    The use Effect

---
+++ native-useEffect/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState,useEffect } from 'react'
+
+function App() {
+
+  const [names,setNames] = useState([]);
+
+  useEffect(() => {
+    fetch("/names.json")
+    .then((respose) => respose.json())
+    .then((data) => setNames(data));
+  },[]);
+
+  return (
+    <div>Names : {names.join(", ")}</div>
+  )
+}
+
+export default App
