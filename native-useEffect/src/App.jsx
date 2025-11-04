b8a2a2600aa646220fc6ca22e0ec18605d47697c
Ari007m <ariharasudan007@gmail.com>
2025-11-04 18:18:44 +0530

    useRef started

--- native-useEffect/src/App.jsx
+++ native-useEffect/src/App.jsx
@@ -1,5 +1,20 @@
 import { useState,useEffect } from 'react'
 
+const StopWatch = ()=>{
+  const [time,setTime] = useState(0);
+
+  useEffect(() => {
+    const intravel = setInterval(() => {
+      setTime((t) => {
+        console.log(t); 
+        return t+1;
+      });
+      return () => clearInterval(intravel);
+    },1000)
+  },[])
+
+  return <div>Time : {time}</div>;
+}
 function App() {
 
   const [names,setNames] = useState([]);
@@ -10,8 +25,25 @@ function App() {
     .then((data) => setNames(data));
   },[]);
 
+  const[selectedName,setSelectedName] = useState();
+  const [details,setDetails] = useState();
+
+  useEffect(() => {
+    fetch(`/${selectedName}.json`)
+    .then((respose) => respose.json())
+    .then((data) => setDetails(data));
+  },[selectedName]);
+
   return (
-    <div>Names : {names.join(", ")}</div>
+    <div>
+      <StopWatch/>
+      <div>
+        {names.map((name) =>(
+          <button onClick={() => setSelectedName(name)}>{name}</button>
+        ))}
+      </div>
+      <div>{JSON.stringify(details)}</div>
+    </div>
   )
 }
 
