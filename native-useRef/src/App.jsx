25fb5e343c035adaa358a1542e5badddb211ff30
Ari007m <ariharasudan007@gmail.com>
2025-11-05 17:41:40 +0530

    useRef finished

--- native-useRef/src/App.jsx
+++ native-useRef/src/App.jsx
@@ -1,12 +1,47 @@
-import { useState } from 'react'
-import './App.css'
+import { useRef,useEffect, useState } from 'react'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const inputRef = useRef(null); 
+  
+  const initialValue =[
+    {id: 1, name:"Ari"},
+    {id: 2, name:"John"}
+  ];
+  const idRef = useRef(initialValue.length+1);
+
+  const [names,setNames] = useState(() => initialValue);
+
+  useEffect(()=>{
+    inputRef.current.focus();
+  },[]);
+
+  const addName = () =>{
+    const val = inputRef.current?.value?.trim();
+    if (!val) return;     
+    setNames((prev) => [...prev, { id: idRef.current++, name: val }]);
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  };
+
+  const onKeyDown =(e) => {
+    if(e.key =='Enter') addName();
+  }
 
   return (
-    <div></div>
-  )
+    <div>
+      <div>
+        {names.map((name) => (
+          <div key={name.id}>{name.id} - {name.name}</div>
+        ))}
+      </div>
+      <div>
+        <input type="text" ref={inputRef} onKeyDown={onKeyDown}/>
+        <button onClick={addName}>ADD</button>
+      </div>
+    </div>
+  );
 }
 
 export default App
